ff8d0ccbab450bbceb7ad6933ec20bc216c9a6a9
Guest User <Guest@Epicodus-7B.local>
2018-02-28 19:36:26 -0800

    Add project

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('form').submit(function(event) {
+
+    var item1 = $('#order1').val();
+    var item2 = $('#order2').val();
+    var item3 = $('#order3').val();
+    var items = [item1,item2,item3];
+    var upperItems = [];
+
+    items.forEach(function(item) {
+      upperItems.push(item.toUpperCase)
+    });
+    var sortItems = upperItems.sort()
+      $("#list").append('<li>' + items + '</li>');
+      event.preventDefault();
+  });
+});
